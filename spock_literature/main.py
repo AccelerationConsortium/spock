74b808fa1c42c985a3ae73dc5208a7cf27b66b41
youssefbriki1 <youssef.briki05@gmail.com>
2024-07-02 12:14:40 -0400

    update

--- spock_literature/main.py
+++ spock_literature/main.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         try:
             author_filled = Author(author)
             print('Author created successfully for ' + author)
-            author_filled.setup_author('json/output.json')
+            author_filled.setup_author('json/output.json', llm=bot_llm)
             print(f"Topics for {author} have been updated")
         except Exception as e:
             print(f"Couldn't find the google scholar profile for {author}: {e}")
