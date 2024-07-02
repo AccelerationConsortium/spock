69d057988940ee6fa269322b8a9a8684f2fdb672
youssefbriki1 <youssef.briki05@gmail.com>
2024-07-02 12:55:11 -0400

    Update

--- spock_literature/main.py
+++ spock_literature/main.py
@@ -19,4 +19,24 @@ if __name__ == "__main__":
         except Exception as e:
             print(f"Couldn't find the google scholar profile for {author}: {e}")
         
-        
+    ### RAG
+    
+    with open('json/output.json','r') as file:
+        data = json.load(file)
+    for author in data:
+        if data[author]['pdf'] != None:
+            try:
+                bot_llm.rag(data[author]['pdf']) 
+                print(f"RAG for {author} pdf generated successfully")
+            except Exception as e:
+                print(f"Couldn't generate the RAG for {author} PDF : {e}")
+        else:
+            try:
+                bot_llm.rag(data[author]['abstract']) 
+                print(f"RAG for {author} abstract generated successfully")
+            except Exception as e:
+                print(f"Couldn't generate the RAG for {author} abstract: {e}")
+                
+                
+    ### Query 
+                      
