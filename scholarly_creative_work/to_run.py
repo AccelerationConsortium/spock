db9442c2d8975bd04467fffa79d716716e4cdef4
youssefbriki1 <youssef.briki05@gmail.com>
2024-09-16 15:12:43 -0400

    update

--- scholarly_creative_work/to_run.py
+++ scholarly_creative_work/to_run.py
@@ -1,6 +1,7 @@
 from LLM import LLM
 import os
 import json
+from utilities import Bot_LLM
 
 
 pdf_list = os.listdir("papers/papers")
@@ -8,48 +9,89 @@ print(pdf_list)
 response = {}
 format_instruction = "Output  either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer."
 for i in range(10):
-    response[i] = {}
-    llm = LLM()
-    llm.set_folder_path("db/db"+str(i))
+    response[pdf_list[i].split('/')[-1].replace("_","/")] = {}
+    llm = Bot_LLM(folder_path='db/db'+str(index))
+    llm.chunk_indexing(file)    
+
+
+    
+    #llm.set_folder_path("db/db"+str(i))
     
     
     print("running split_and_embedding_chunk_pdf")
     llm.split_and_embedding_chunk_pdf("papers/papers/"+pdf_list[i])
-    response[i]['affiliation'] = llm.query_rag("What are the authors affiliation. Output a dictionary ? Output  either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+    # Process 'affiliation'
+    output_llm = llm.query_rag("What are the authors affiliation. Output a dictionary ? Output either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+    response[i]['affiliation'] = {'Yes/No': output_llm.split('/')[0].strip(), 'sentence': output_llm.split('/')[1]}
     print("-----")
-    response[i]['topic']  = llm.query_rag("What are topics of the paper ?Output  either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
-    print("-----")
-    response[i]['new materials'] = llm.query_rag("does the article mention any new material discovery ? Output  either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
-    print("-----")
-    response[i]['screening algorithms'] = llm.query_rag("A screening algorithm is a systematic procedure or method used to identify individuals who may have or be at risk for a particular condition or trait within a large population. These algorithms are designed to quickly and efficiently screen out those who are unlikely to have the condition, while identifying those who may require further diagnostic evaluation or intervention. If there are any, What are the screening algorithms used in the paper ? Output  either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
-    print("-----")
-    response[i]['AI algorithms'] = llm.query_rag("AI algorithms are computational methods and processes used to solve specific tasks by mimicking human intelligence. These algorithms enable machines to learn from data, make decisions, and perform tasks that typically require human intelligence. Output  either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
-    print("-----")
-    
-    response[i]['workflow'] = llm.query_rag("Can you describe to me the workflow used by the author ? Output  either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
-    print("-----")
-    response[i]['methods'] = llm.query_rag("can you do a methods description ? Output  either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
-    print("-----")
-    response[i]['models'] = llm.query_rag("what are the models used in the article ? Output  either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
-    print("-----")
-    response[i]['funding'] = llm.query_rag("does the article mention who funded it. Output  either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
 
-    #response[i]['new composition of matter'] = llm.query_rag("Has the article discovered any new composition of matter? If yes, please provide details.")
-    #print("-----")
-    #response[i]['AI or screening algorithms'] = llm.query_rag("Does the article describe any new AI or screening algorithms, methods, workflows, or models? If yes, provide details.")
-    #print("-----")
-    #response[i]['experimental methodologies'] = llm.query_rag("Are there any new experimental methodologies published in the article? Please provide a description.")
-    #print("-----")
-    response[i]['material datasets'] = llm.query_rag("Does the article share any AI or material-related datasets? If yes, provide the details. Output  either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+    # Process 'topic'
+    output_llm = llm.query_rag("What are topics of the paper? Output the topic of the paper '/' then a sentence from the document that supports your answer.") # To updatee
+    response[i]['topic'] =  output_llm.split('/')[0].strip()
+    response[i]['topic'].update({'sentence': output_llm.split('/')[1]})
     print("-----")
-    response[i]['drug formulations explored'] = llm.query_rag("Has the article explored any new drug formulations? If yes, what are they? Output  either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+
+    # Process 'new materials'
+    output_llm = llm.query_rag("Does the article mention any new material discovery? Output either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+    response[i]['new materials'] = {'Yes/No': output_llm.split('/')[0].strip(), 'sentence': output_llm.split('/')[1]}
     print("-----")
-    response[i]['novel drug formulations'] = llm.query_rag("Does the article identify any novel drug formulations? If yes, provide the details. Output  either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+
+    # Process 'screening algorithms'
+    output_llm = llm.query_rag("A screening algorithm is a systematic procedure or method used to identify individuals who may have or be at risk for a particular condition or trait within a large population. These algorithms are designed to quickly and efficiently screen out those who are unlikely to have the condition, while identifying those who may require further diagnostic evaluation or intervention. If there are any, what are the screening algorithms used in the paper? Output either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+    response[i]['screening algorithms'] = {'Yes/No': output_llm.split('/')[0].strip(), 'sentence': output_llm.split('/')[1]}
     print("-----")
-    response[i]['lead small-molecule drug candidates'] = llm.query_rag("Does the article mention any lead small-molecule drug candidates? If so, what are they? Output  either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+
+    # Process 'AI algorithms'
+    output_llm = llm.query_rag("AI algorithms are computational methods and processes used to solve specific tasks by mimicking human intelligence. These algorithms enable machines to learn from data, make decisions, and perform tasks that typically require human intelligence. Output either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+    response[i]['AI algorithms'] = {'Yes/No': output_llm.split('/')[0].strip(), 'sentence': output_llm.split('/')[1]}
     print("-----")
-    response[i]['clinical trials'] = llm.query_rag("Are there any clinical trials mentioned in the article? If yes, provide the details. Output  either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+
+    # Process 'workflow'
+    '''
+    output_llm = llm.query_rag("Can you describe to me the workflow used by the author? Output either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+    response[i]['workflow'] = {'Yes/No': output_llm.split('/')[0].strip(), 'sentence': output_llm.split('/')[1]}
     print("-----")
+
+    # Process 'methods'
+    output_llm = llm.query_rag("Can you do a methods description? Output either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+    response[i]['methods'] = {'Yes/No': output_llm.split('/')[0].strip(), 'sentence': output_llm.split('/')[1]}
+    print("-----")
+    '''
+    # Process 'models'
+    output_llm = llm.query_rag("What are the models used in the article? Output either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+    response[i]['models'] = {'Yes/No': output_llm.split('/')[0].strip(), 'sentence': output_llm.split('/')[1]}
+    print("-----")
+
+    # Process 'funding'
+    output_llm = llm.query_rag("Does the article mention who funded it? Output either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+    response[i]['funding'] = {'Yes/No': output_llm.split('/')[0].strip(), 'sentence': output_llm.split('/')[1]}
+    print("-----")
+
+    # Process 'material datasets'
+    output_llm = llm.query_rag("Does the article share any AI or material-related datasets? If yes, provide the details. Output either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+    response[i]['material datasets'] = {'Yes/No': output_llm.split('/')[0].strip(), 'sentence': output_llm.split('/')[1]}
+    print("-----")
+
+    # Process 'drug formulations explored'
+    output_llm = llm.query_rag("Has the article explored any new drug formulations? If yes, what are they? Output either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+    response[i]['drug formulations explored'] = {'Yes/No': output_llm.split('/')[0].strip(), 'sentence': output_llm.split('/')[1]}
+    print("-----")
+
+    # Process 'novel drug formulations'
+    output_llm = llm.query_rag("Does the article identify any novel drug formulations? If yes, provide the details. Output either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+    response[i]['novel drug formulations'] = {'Yes/No': output_llm.split('/')[0].strip(), 'sentence': output_llm.split('/')[1]}
+    print("-----")
+
+    # Process 'lead small-molecule drug candidates'
+    output_llm = llm.query_rag("Does the article mention any lead small-molecule drug candidates? If so, what are they? Output either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+    response[i]['lead small-molecule drug candidates'] = {'Yes/No': output_llm.split('/')[0].strip(), 'sentence': output_llm.split('/')[1]}
+    print("-----")
+
+    # Process 'clinical trials'
+    output_llm = llm.query_rag("Are there any clinical trials mentioned in the article? If yes, provide the details. Output either 'Yes' or 'No' followed by a '/' then a sentence from the document that supports your answer.")
+    response[i]['clinical trials'] = {'Yes/No': output_llm.split('/')[0].strip(), 'sentence': output_llm.split('/')[1]}
+    print("-----")
+
     print(response)
 
 with open("llm_ouput.json", "w") as f:
