afcd1fcfd554d6f38b011cfe0cd960accf693237
youssefbriki1 <youssef.briki05@gmail.com>
2024-09-18 23:32:48 -0400

    update

---
+++ scholarly_creative_work/executable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=12:00:0
+####################################################
+##########MACHINE SPECIFIC DETAILS GO HERE##########
+####################################################
+
+module load BalamEnv
+cd /home/m/mehrad/brikiyou/scratch/spock_package
+source bin/activate
+cd spock/scholarly_creative_work
+
+python3 run.py "$name"
+
+rm -rf slurm*
