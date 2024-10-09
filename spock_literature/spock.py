e0412a1249f36ad6a781983bf1cc9543d1a10a59
youssefbriki1 <youssef.briki05@gmail.com>
2024-10-09 15:20:10 -0400

    Update

--- spock_literature/spock.py
+++ spock_literature/spock.py
@@ -5,25 +5,13 @@ import concurrent.futures
 from publication import Publication
 from author import Author
 
-author = Author('Mehrad Ansari')
-print(author.get_last_publication())
-"""
-pub = Publication(author.get_last_publication())
-print(pub)
-print(pub.title)
-print(pub.abstract)
-print(pub.topic)
 
 
-while True:
-        
-    with open('json/ouput.json', 'r') as file:
-        scholars_publications = json.load(file)
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:  # Adjust max_workers as needed
-        executor.map(process_scholar, scholars_publications.items())
+class Spock(): # Heritage a voir plus tard
+    """Spock class."""
+    def __init__(self):
+        """Initialize Spock."""
+        pass
     
-
-    print('Waiting!')
-    time.sleep(900)
-"""
+    
+    def 
