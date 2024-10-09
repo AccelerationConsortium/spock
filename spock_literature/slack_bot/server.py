6246299390b333a44106bb31795504b39b4d6bb9
youssefbriki1 <youssef.briki05@gmail.com>
2024-10-08 22:14:41 -0400

    working on slack bot

--- spock_literature/slack_bot/server.py
+++ spock_literature/slack_bot/server.py
@@ -1,110 +1,108 @@
-'''
-Main file for the Slack bot.
-
-
-
-Need to remove Flask from here
-
-'''
-
 import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
-from slack_bolt.adapter.flask import SlackRequestHandler
-from slack_bolt import App
-from dotenv import find_dotenv, load_dotenv
-from flask import Flask, request
-from functions import draft_email
-
-import os
+from bot import Bot
+import concurrent.futures
 from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
-from slack_bolt.adapter.flask import SlackRequestHandler
+from slack_sdk.socket_mode import SocketModeClient
+from slack_sdk.socket_mode.request import SocketModeRequest
+from slack_sdk.socket_mode.response import SocketModeResponse
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
 from slack_bolt import App
-from dotenv import find_dotenv, load_dotenv
-from flask import Flask, request
-from functions import draft_email
 
-# Load environment variables from .env file
-load_dotenv(find_dotenv())
+# Your Slack bot token
+slack_token = 'xoxb-1089129130001-7130503874147-1eJXyg9HdahYaxOHANd8iyc0'
 
-# Set Slack API credentials
-SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
-SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
-SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
+# Initialize a Web API client
+client = WebClient(token=slack_token)
 
-# Initialize the Slack app
-app = App(token=SLACK_BOT_TOKEN)
+# The channel ID of the private channel you want to send the message to
+# The message you want to send
+message = 'File was changed'
 
-# Initialize the Flask app
-# Flask is a web application framework written in Python
-flask_app = Flask(__name__)
-handler = SlackRequestHandler(app)
+bot = Bot('test.txt')
+socket_mode_client = SocketModeClient(app_token="xapp-1-A074H63F6EL-7837148342518-b5be6962fadd66118e88e8aa2c1cdca5f393404870ef5d49ee25d944fc6fc02b", web_client=client)
+app = App(token=slack_token)
 
 
-def get_bot_user_id():
-    """
-    Get the bot user ID using the Slack API.
-    Returns:
-        str: The bot user ID.
-    """
-    try:
-        # Initialize the Slack client with your bot token
-        slack_client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
-        response = slack_client.auth_test()
-        return response["user_id"]
-    except SlackApiError as e:
-        print(f"Error: {e}")
+@app.event("file_shared")
+def file_func(payload, client, ack):
+    print("okay")
+    ack()
+
+    #get the file id every time someone uploads a pdf
+    my_file = payload.get('file').get('id')
+    
+    #get the json using files_info
+    url = client.files_info(file = my_file).get('file').get('url_private')
+    file_name = client.files_info(file = my_file).get('file').get('title')
+
+    # save file
+    resp = requests.get(url, headers={'Authorization': 'Bearer %s' % token})
+    save_file = Path(file_name)
+    save_file.write_bytes(resp.content)
+@app.command("/hello")
+def hello_command(ack, body):
+    print("hello")
+    user_id = body["user_id"]
+    ack(f"Hi, <@{user_id}>!")
 
 
-def my_function(text):
-    """
-    Custom function to process the text and return a response.
-    In this example, the function converts the input text to uppercase.
+def process_slash_command(payload):
+    global bot
+    command = payload['command']
+    user_id = payload['user_id']
+    text = payload['text']
+    channel_id = payload['channel_id']
 
-    Args:
-        text (str): The input text to process.
+    if command == '/lastmodified':
+        response_message = f"Hello <@{user_id}>! You invoked the command with text: {text} and the latest modified time is {bot.last_modified()}"
 
-    Returns:
-        str: The processed text.
-    """
-    response = text.upper()
-    return response
-
-
-@app.event("app_mention")
-def handle_mentions(body, say):
-    """
-    Event listener for mentions in Slack.
-    When the bot is mentioned, this function processes the text and sends a response.
-
-    Args:
-        body (dict): The event data received from Slack.
-        say (callable): A function for sending a response to the channel.
-    """
-    text = body["event"]["text"]
-
-    mention = f"<@{SLACK_BOT_USER_ID}>"
-    text = text.replace(mention, "").strip()
-
-    say("Sure, I'll get right on that!")
-    # response = my_function(text)
-    response = draft_email(text)
-    say(response)
-
-
-@flask_app.route("/slack/events", methods=["POST"])
-def slack_events():
-    """
-    Route for handling Slack events.
-    This function passes the incoming HTTP request to the SlackRequestHandler for processing.
-
-    Returns:
-        Response: The result of handling the request.
-    """
-    return handler.handle(request)
+        try:
+            # Post the message
+            client.chat_postMessage(
+                channel=channel_id,
+                text=response_message
+            )
+            print("/lastdate was successfully posted")
+        except SlackApiError as e:
+            print(f"Error posting message: {e.response['error']}")
+            
+    elif command == '/stop':
+        response_message = f"Hello <@{user_id}>! You invoked the command with text: {text} and it's stopping the bot"
+        bot.stop()
+        try:
+            # Post the message
+            client.chat_postMessage(
+                channel=channel_id,
+                text=response_message
+            )
+            print("/stop was successfully posted")
+        except SlackApiError as e:
+            print(f"Error posting message: {e.response['error']}")
+    elif command == '/start':
+        bot.running = True
+        response_message = f"Hello <@{user_id}>! You invoked the command with text: {text} and it's starting the bot"
+        try:
+            # Post the message
+            client.chat_postMessage(
+                channel=channel_id,
+                text=response_message
+            )
+            print("/start was successfully posted")
+        except SlackApiError as e:
+            print(f"Error posting message: {e.response['error']}")
 
 
-# Run the Flask app
+# Function to handle incoming Socket Mode requests
+def handle_socket_mode_request(client: SocketModeClient, req: SocketModeRequest):
+    if req.type == "slash_commands":
+        process_slash_command(req.payload)
+        client.send_socket_mode_response(SocketModeResponse(envelope_id=req.envelope_id))
+
+# Register the handler to the client
+#socket_mode_client.socket_mode_request_listeners.append(handle_socket_mode_request)
+
 if __name__ == "__main__":
-    flask_app.run()
+    SocketModeHandler(app, "xapp-1-A074H63F6EL-7133121371428-665e0a091a6bbdab0068fdfa9a939f66e538a35faa9cbd6db6667cf6d21c6d52" ).start()
