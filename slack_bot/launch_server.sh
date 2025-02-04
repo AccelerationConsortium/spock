9338ceabe380f875168fab8587e1468e4c0fbd7a
Youssef Briki <youssef.briki@umontreal.ca>
2025-02-04 15:26:14 -0500

    Updated script + github workflow

--- slack_bot/launch_server.sh
+++ slack_bot/launch_server.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=0
-#SBATCH --time=1:00:00
+#SBATCH --time=24:00:00
 #SBATCH --output=/home/m/mehrad/brikiyou/scratch/spock/slack_bot/out/slurm-%j.out
 #SBATCH --error=/home/m/mehrad/brikiyou/scratch/spock/slack_bot/out/slurm-%j.err
-#SBATCH -p debug
 
 module load BalamEnv
 module load python
 source /home/m/mehrad/brikiyou/scratch/new_spock_venv_2/bin/activate
-python3 /home/m/mehrad/brikiyou/scratch/spock/slack_bot/server.py
+
+timeout 23h python3 /home/m/mehrad/brikiyou/scratch/spock/slack_bot/server.py
+exit_code=$?
+
+if [[ $exit_code -eq 124 ]]; then
+    echo "$(date): Job timed out. Resubmitting..."
+    sbatch "$0"
+else
+    echo "$(date): Job completed or stopped before timeout."
+fi
