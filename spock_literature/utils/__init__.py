c295c7fb87b87ab74059e77994c709b76c1e65da
Youssef Briki <youssef.briki@umontreal.ca>
2025-06-22 23:06:40 -0400

    Fixed publication

---
+++ spock_literature/utils/__init__.py
@@ -0,0 +1,9 @@
+import importlib
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from spock_literature.utils import Spock_GraphGenerator
+    from spock_literature.utils import Helper_LLM
+    from spock_literature.utils import Spock_Publication
+    from spock_literature.utils import Spock_PDFLoader
+    
